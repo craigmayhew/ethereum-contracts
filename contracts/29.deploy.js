2e303f8b62da5d0e8fea51e16da3845ae74a4bcd
Craig Mayhew <craig@mayhew.io>
2018-12-28 22:39:49 +0000

    Further fixes as we are now using web3 v1.0beta

--- contracts/29.deploy.js
+++ contracts/29.deploy.js
@@ -27,9 +27,9 @@ web3.eth.net.isListening()
         gas: 1000000
     }).then(function (contract29) {
         //console.log(contract29.address) // the contract address
-        console.log(contract29) // the contract
-        console.log("Sending prize fund ether to 29.sol on rinkeby to: ", contract29.address);
-        web3.eth.sendTransaction({from:ethAccount, to:contract29.address, value: 555529000})
+        console.log(contract29.options) // the contract
+        console.log("Sending prize fund ether to 29.sol on rinkeby to: ", contract29.options.address);
+        web3.eth.sendTransaction({from:ethAccount, to:contract29.options.address, value: 555529000})
         .then(function (txnHash) {
             // now you have the unmined transaction hash, return receipt promise
             console.log(txnhash); // follow along
@@ -37,8 +37,8 @@ web3.eth.net.isListening()
         })
         .then(function (receipt) {
             console.log("Send correct answer for 29.sol")
-            let getData = contract29.attempt.getData(2220422932,-2128888517,-283059956)
-            return web3.eth.sendTransaction({from:ethAccount, to:contract29.address, data: getData})
+            let getData = contract29.methods.attempt.getData(2220422932,-2128888517,-283059956)
+            return web3.eth.sendTransaction({from:ethAccount, to:contract29.options.address, data: getData})
         })
     })
 });
