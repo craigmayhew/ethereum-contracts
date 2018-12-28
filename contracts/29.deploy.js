49e064baba5206d5635f4754cab92380ae95ff07
Craig Mayhew <craig@mayhew.io>
2018-12-28 21:52:18 +0000

    Refactored to use promises for grabbing the account

--- contracts/29.deploy.js
+++ contracts/29.deploy.js
@@ -9,34 +9,36 @@ let storageOutput = fs.readFileSync('/tmp/29.compiled.js', 'utf8');
 //convert the output from a string to a javascript object
 storageOutput = JSON.parse(storageOutput);
 
-console.log('web3.eth.getAccounts():',web3.eth.getAccounts());
-web3.eth.getAccounts().then(e => console.log(e[0]));
-
 web3.eth.net.isListening()
-.then(() => console.log('web3 is connected, accounts: ', web3.eth.accounts))
-.catch(e => console.log('ERROR connection not made to local node /home/travis/.ethereum/rinkeby/geth.ipc'));
+.then(function(e) {
+    console.log('web3 is connected on ipc');
+    return web3.eth.getAccounts();
+}).then(function(e) {
+    console.log('account: ',e[0]);
+    let ethAccount = e[0];
+    let storageContractAbi = storageOutput.contracts['contracts/29.sol:ethForAnswersBounty'].abi
+    let storageContract = new web3.eth.Contract(JSON.parse(storageContractAbi))
+    let storageBinCode = "0x" + storageOutput.contracts['contracts/29.sol:ethForAnswersBounty'].bin
+    storageContract.deploy({
+        data: storageBinCode,
+        arguments: [29]
+    }).send({
+        from: ethAccount,
+        gas: 1000000
+    }).then(function (contract29) {
+        //console.log(contract29.address) // the contract address
+        console.log("Sending prize fund ether to 29.sol on rinkeby to: ", contract29.address)
+        eth.sendTransaction({from:ethAccount, to:contract29.address, value: 555529000})
+        .then(function (txnHash) {
+            // now you have the unmined transaction hash, return receipt promise
+            console.log(txnhash); // follow along
+            return web3.eth.getTransactionReceiptMined(txnHash);
+        })
+        .then(function (receipt) {
+            console.log("Send correct answer for 29.sol")
+            let getData = contract29.attempt.getData(2220422932,-2128888517,-283059956)
+            return web3.eth.sendTransaction({from:ethAccount, to:contract29.address, data: getData})
+        })
+    })
+});
 
-let storageContractAbi = storageOutput.contracts['contracts/29.sol:ethForAnswersBounty'].abi
-let storageContract = new web3.eth.Contract(JSON.parse(storageContractAbi))
-let storageBinCode = "0x" + storageOutput.contracts['contracts/29.sol:ethForAnswersBounty'].bin
-storageContract.deploy({
-    data: storageBinCode,
-    arguments: [29]
-}).send({
-    from: web3.eth.accounts[0],
-    gas: 1000000
-}).then(function (contract29) {
-    //console.log(contract29.address) // the contract address
-    console.log("Sending prize fund ether to 29.sol on rinkeby to: ", contract29.address)
-    eth.sendTransaction({from:web3.eth.accounts[0], to:contract29.address, value: 555529000})
-    .then(function (txnHash) {
-        // now you have the unmined transaction hash, return receipt promise
-        console.log(txnhash); // follow along
-        return web3.eth.getTransactionReceiptMined(txnHash);
-    })
-    .then(function (receipt) {
-        console.log("Send correct answer for 29.sol")
-        let getData = contract29.attempt.getData(2220422932,-2128888517,-283059956)
-        return web3.eth.sendTransaction({from:web3.eth.accounts[0], to:contract29.address, data: getData})
-    })
-})
