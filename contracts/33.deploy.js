4dcbab01d151d065859efc7a54bce363da27536f
Craig Mayhew <craig@mayhew.io>
2018-12-30 00:13:32 +0000

    Added loop to allow testing multiple versions of the 33 contract e.g. 29 and 30.

--- contracts/33.deploy.js
+++ contracts/33.deploy.js
@@ -12,44 +12,50 @@ let storageOutput = fs.readFileSync('/tmp/33.compiled.js', 'utf8');
 //convert the output from a string to a javascript object
 storageOutput = JSON.parse(storageOutput);
 
-web3.eth.net.isListening()
-.then(function(e) {
-    console.log('web3 is connected on ipc');
-    return web3.eth.getAccounts();
-}).then(function(e) {
-    console.log('account: ',e[0]);
-    let ethAccount = e[0];
-    let storageContractAbi = storageOutput.contracts['contracts/33.sol:ethForAnswersBounty'].abi
-    let storageContract = new web3.eth.Contract(JSON.parse(storageContractAbi))
-    let storageBinCode = "0x" + storageOutput.contracts['contracts/33.sol:ethForAnswersBounty'].bin
-    storageContract.deploy({
-        data: storageBinCode,
-        arguments: [29]
-    }).send({
-        from: ethAccount,
-        gas: 1000000
-    }).then(function (contract33) {
-        console.log(" . Sending prize fund ether to 33.sol (answer=29) on rinkeby to: ", contract33.options.address);
-        web3.eth.sendTransaction({from:ethAccount, to:contract33.options.address, value: 29000})
-        .then(function (txnHash) {
-            // now you have the unmined transaction hash, return receipt promise
-            console.log(" ✔ Sent prize fund ether to 33.sol (answer=29) on rinkeby. transaction: "+txnHash.transactionHash);
-            return web3.eth.getTransactionReceiptMined(txnHash.transactionHash);
-        })
-        .then(function (receipt) {
-            console.log(" . Sending correct answer for 33.sol (answer=29)")
-            contract33.methods.attempt(3,1,1).send({
-                from: ethAccount,
-                gas: 1000000
-            })
-            .then(function(receipt){
-                console.log(" ✔ Sent correct answer for 33.sol (answer=29)")
-            })
-            .then(function(){
-                //TODO: ✔✘ check we have the correct (one) number of transactions from the contract using web3.eth.getTransactionCount
-                process.exit(0);
-            });
-        })
-    })
-});
+const answers = {
+    ans24: [24,-2901096694, -15550555555, 15584139827],
+    ans29: [29,3,1,1]
+}
 
+for (let ans in answers) {
+    web3.eth.net.isListening()
+    .then(function(e) {
+        console.log('web3 is connected on ipc');
+        return web3.eth.getAccounts();
+    }).then(function(e) {
+        console.log('account: ',e[0]);
+        let ethAccount = e[0];
+        let storageContractAbi = storageOutput.contracts['contracts/33.sol:ethForAnswersBounty'].abi
+        let storageContract = new web3.eth.Contract(JSON.parse(storageContractAbi))
+        let storageBinCode = "0x" + storageOutput.contracts['contracts/33.sol:ethForAnswersBounty'].bin
+        storageContract.deploy({
+            data: storageBinCode,
+            arguments: [answers[ans][0]]
+        }).send({
+            from: ethAccount,
+            gas: 1000000
+        }).then(function (contract33) {
+            console.log(" . Sending prize fund ether to 33.sol (answer="+answers[ans][0]+") on rinkeby to: ", contract33.options.address);
+            web3.eth.sendTransaction({from:ethAccount, to:contract33.options.address, value: 29000})
+            .then(function (txnHash) {
+                // now you have the unmined transaction hash, return receipt promise
+                console.log(" ✔ Sent prize fund ether to 33.sol (answer="+answers[ans][0]+") on rinkeby. transaction: "+txnHash.transactionHash);
+                return web3.eth.getTransactionReceiptMined(txnHash.transactionHash);
+            })
+            .then(function (receipt) {
+                console.log(" . Sending correct answer for 33.sol (answer="+answers[ans][0]+")")
+                contract33.methods.attempt(answers[ans][1],answers[ans][2],answers[ans][3]).send({
+                    from: ethAccount,
+                    gas: 1000000
+                })
+                .then(function(receipt){
+                    console.log(" ✔ Sent correct answer for 33.sol (answer="+answers[ans][0]+")")
+                })
+                .then(function(){
+                    //TODO: ✔✘ check we have the correct (one) number of transactions from the contract using web3.eth.getTransactionCount
+                    //process.exit(0);
+                });
+            })
+        })
+    });
+}
